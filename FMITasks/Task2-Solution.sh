021ee19982af2e8bbbe026c56dfceab31464008d
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-04-24 14:56:08 +0300

    Update Task2-Solution.sh

--- FMITasks/Task2-Solution.sh
+++ FMITasks/Task2-Solution.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-	echo "Invalid number of arguments!"
-	exit 1
-fi
-
-DIR_PATH="${1}"
-
-if [ ! -d "${DIR_PATH}" ]; then
-	echo "Argument is not a directory!"
-	exit 2
-elif [ ! -r "$DIR_PATH" ]; then
-	echo "Directory is not readable!"
-	exit 3
-fi
-
-find -L "${DIR_PATH}" -type l 2>/dev/null -print0 | xargs -0 -I {} basename {}
-svetlinpopivanov@Air~$ cat task14.sh
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
 	echo "Error: expected 1 argument!"
 	exit 1
 fi
