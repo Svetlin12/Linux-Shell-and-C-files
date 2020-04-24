161bfea247854fac351799835ce64ba3b406e60e
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-04-24 21:25:05 +0300

    Create Тask4-Soltion.sh

---
+++ FMITasks/Тask4-Soltion.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 1 -a $# -ne 2 ]; then
+	echo "error: expected one or two arguments as input"
+	exit 1
+fi
+
+if [ ! -d "$1" ]; then
+	echo "error: directory does not exist"
+	exit 2
+fi
+
+if [ ! -r "$1" ]; then
+	echo "error: directory is not readable"
+	exit 3
+fi
+
+DIRECTORY=$1
+NUMBER=$2
+
+if [ $# -eq 2 ]; then
+	while read NUMHARDLINKS FILENAME; do
+		if [ ${NUMHARDLINKS} -ge ${NUMBER} ]; then
+			echo "${FILENAME}"
+		fi
+	done < <(gfind "${DIRECTORY}" -type f 2>/dev/null -printf "%n %p\n")   
+elif [ $# -eq 1 ]; then
+	find -L "${DIRECTORY}" -type l 2>/dev/null -print0 | xargs -0 -I {} basename {}
+fi
