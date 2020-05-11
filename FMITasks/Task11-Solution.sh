9e4d7a3eabc6ada7881966deeb8e6c6a2af06e34
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-05-11 20:40:37 +0300

    Create Task11-Solution.sh

---
+++ FMITasks/Task11-Solution.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+	echo "expected root as caller"
+	exit 1
+fi
+
+if [ $# -ne 1 ]; then
+	echo "usage: ./task17.sh USERNAME"
+	exit 2
+fi
+
+FOO=$1
+
+if [ $(grep -c "${FOO}" <(cat /etc/passwd | cut -d : -f 1)) == 0 ]; then
+	echo "invalid username"
+	exit 3
+fi
+
+### a)
+
+FOOPCOUNT=$(ps -u "${FOO}" | wc -l)
+
+while read _USER; do
+	USERPCOUNT=$(ps -u "${_USER}" | wc -l)
+	
+	if [ "${USERPCOUNT}" -gt "${FOOPCOUNT}" ]; then
+		echo "${_USER}"
+	fi
+
+done < <(cat /etc/passwd | cut -d : -f 1)
+
+### b)
+
+AVG=$(awk '{cnt++; time += $1} END{ print time/cnt }' <(ps -e -o etimes=))
+echo "${AVG}"
+
+### c)
+
+while read PID PROCTIME; do
+
+	if [ $(echo "${PROCTIME} > $(echo "${AVG} * 2" | bc)" | bc) -eq 1 ]; then
+		echo "pid: ${PID} etimes: ${PROCTIME}"
+		# kill -9 ${PID}
+		# sleep 2
+		# kill -15 ${PID}
+	fi
+
+done < <(ps -u "${FOO}" -o pid=,etimes=)
