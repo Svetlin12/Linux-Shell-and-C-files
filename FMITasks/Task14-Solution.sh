76162dd571006c0f198c51b81ce379435898aedb
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-05-12 18:39:14 +0300

    Create Task14-Solution.sh

---
+++ FMITasks/Task14-Solution.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ $# -ne 1 -a $# -ne 2 ]; then
+        echo "usage: ./task14.sh dirname [filename]"
+	exit 1
+fi
+
+DIRNAME=$1
+
+if [ ! -d "${DIRNAME}" ]; then
+	echo "${DIRNAME} is not a directory"
+	exit 2
+fi
+
+if [ ! -r "${DIRNAME}" ]; then
+	echo "${DIRNAME} is not readable"
+	exit 3
+fi
+
+if [ $# -eq 2 ]; then
+	FILENAME=$2
+fi
+
+if [ -n "${FILENAME}" -a ! -f "${FILENAME}" ]; then
+	echo "${FILENAME} is not a file"
+	exit 4
+fi
+
+if [ -n "${FILENAME}" -a ! -w "${FILENAME}" ]; then
+	echo "${FILENAME} is not readable"
+	exit 5
+fi
+
+BROKENSYMLINKS=$(find "${DIRNAME}" -type l ! -exec test -e {} \; -print | wc -l);
+SYMLINKS=$(find "${DIRNAME}" -type l -exec test -e {} \; -print)
+DESTINATION=$(find "${DIRNAME}" -type l -exec test -e {} \; -print | xargs -I {} readlink {});
+
+COMBINED=$(paste <(echo "${SYMLINKS}") <(echo "${DESTINATION}"))
+
+while read _SYMLINK _DEST; do
+	if [ -n "${FILENAME}" ]; then
+		echo "${_SYMLINK} -> ${_DEST}" >> "${FILENAME}"
+	else
+		echo "${_SYMLINK} -> ${_DEST}"
+	fi
+done < <(echo "${COMBINED}") 
+
+if [ -n "${FILENAME}" ]; then
+	echo "Broken symlinks: ${BROKENSYMLINKS}" >> "${FILENAME}"
+else
+	echo "Broken symlinks: ${BROKENSYMLINKS}"
+fi
