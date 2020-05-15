df656a2ee00ad674739484ba591ecac9532fdef0
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-05-15 11:43:35 +0300

    Update task40.c

--- C/FMI/task40.c
+++ C/FMI/task40.c
@@ -11,35 +11,46 @@ int main(int argc, char** argv)
 	{
 		char c;
 		int number = (int)(*argv[2]) - '0';
-		int cnt = 1;
+		int cnt = 0;
 
-		if (*argv[2] < '0' || *argv[2] > '9')
-			errx(1, "usage: ./main -c [0-9]");
+		if (*argv[2] < '1' || *argv[2] > '9')
+			errx(1, "usage: ./main -c [1-9]");
 
 		while (read(0, &c, 1) > 0)
 		{
-			if (cnt == number || c == '\n')
-				write(1, &c, sizeof(c));
 			cnt++;
+
+			if (c == '\n')
+			{
+				write(1, "\n", 1);
+				cnt = 0;
+			}
+			else if (cnt == number)
+				write(1, &c, sizeof(c));
 		}
 	}
 	else if (argc == 3 && strcmp(argv[1], "-c") == 0 && strlen(argv[2]) == 3)
 	{
 		if (argv[2][0] > argv[2][2])
-			errx(2, "expected interval: [0-9]-[0-9]");
+			errx(2, "expected interval: [1-9]-[1-9]");
 
-		if (argv[2][0] < '0' || argv[2][0] > '9' || argv[2][2] < '0' || argv[2][2] > '9' || argv[2][1] != '-')
-			errx(3, "usage: ./main -c [0-9]-[0-9]");
+		if (argv[2][0] < '1' || argv[2][0] > '9' || argv[2][2] < '1' || argv[2][2] > '9' || argv[2][1] != '-')
+			errx(3, "usage: ./main -c [1-9]-[1-9]");
 
 		char first = (int)argv[2][0] - '0';
 		char last = (int)argv[2][2] - '0';
 		char c;
-		int cnt = 1;
+		int cnt = 0;
 		while (read(0, &c, 1) > 0)
 		{
-			if ((cnt >= first && cnt <= last) || c == '\n')
-				write(1, &c, sizeof(c));
 			cnt++;
+			if (c == '\n')
+			{
+				write(1, &c, sizeof(c));
+				cnt = 0;
+			}
+			else if (cnt >= first && cnt <= last)
+				write(1, &c, sizeof(c));
 		}
 	}	
 
@@ -48,7 +59,7 @@ int main(int argc, char** argv)
 		if (strcmp(argv[3], "-f") != 0)
 		       errx(4, "expected -f after delimiter");
 		
-		if (*argv[4] < '0' && *argv[4] > '9')
+		if (*argv[4] < '1' || *argv[4] > '9')
 			errx(5, "expected number of column");
 
 		char delimiter = argv[2][0];
@@ -60,18 +71,22 @@ int main(int argc, char** argv)
 		{
 			if (c == delimiter)
 				cnt++;
-			
-			if (cnt == numColumn && c != '\n' && c != delimiter)
+		
+			if (c == '\n')
+			{
+				write(1, "\n", 1);
+				cnt = 1;
+			}
+			else if (cnt == numColumn && c != delimiter)
 				write(1, &c, sizeof(c));
 		}
-		write(1, "\n", 1);
 	}
 	else if (argc == 5 && strcmp(argv[1], "-d") == 0 && strlen(argv[4]) == 3)
 	{
 		if (strcmp(argv[3], "-f") != 0)
 			errx(6, "expected -f after delimiter");
 		
-		if (argv[4][0] < '0' || argv[4][0] > '9' || argv[4][2] < '0' || argv[4][2] > '9' || argv[4][1] != '-')
+		if (argv[4][0] < '1' || argv[4][0] > '9' || argv[4][2] < '1' || argv[4][2] > '9' || argv[4][1] != '-')
 			errx(7, "incorrect interval");
 
 		char delimiter = argv[2][0];
@@ -87,19 +102,23 @@ int main(int argc, char** argv)
 		while (read(0, &c, 1) > 0)
 		{
 			if (c == delimiter)
-			{
 				cnt++;
-				if (cnt == firstColumn)
-					continue;
-			}
 
-			if (cnt >= firstColumn && cnt <= lastColumn && c != '\n')
+			if (c == '\n')
+			{
+				write(1, "\n", 1);
+				cnt = 1;
+			}
+			else if (cnt >= firstColumn && cnt <= lastColumn)
+			{
+				if (cnt == firstColumn && c == delimiter)
+					continue;	
 				write(1, &c, sizeof(c));
+			}
 		}
-		write(1, "\n", 1);
 	}
 	else
-		errx(9, "usage: ./main -c [0-9]\n./main -c [0-9]-[0-9]\n./main -d [char] -f [0-9]\n./main -d [char] -f [0-9]-[0-9]");
+		errx(9, "usage: ./main -c [1-9]\n./main -c [1-9]-[1-9]\n./main -d [char] -f [1-9]\n./main -d [char] -f [1-9]-[1-9]");
 	
 
 	exit(0);
