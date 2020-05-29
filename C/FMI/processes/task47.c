e6169933415c00ba68ebdce18b311c677ffc4f1a
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-05-29 16:03:23 +0300

    Update task47.c

--- C/FMI/processes/task47.c
+++ C/FMI/processes/task47.c
@@ -1,84 +1,96 @@
 #include <err.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+pid_t executeCommand(char** args, int stdIn, int stdOut, int toClose, const char* msg)
+{
+	pid_t pid = fork();
+	if (pid == -1)
+		err(2, "could not fork");
+
+	if (pid == 0)
+	{
+		if (toClose != -1) {
+			close(toClose);
+		}
+		if (stdIn != -1 && dup2(stdIn, 0) == -1)
+			err(4, "could not execute dup2 (%s stdin)", msg);
+
+		if (stdOut != -1 && dup2(stdOut, 1) == -1)
+			err(4, "could not execute dup2 (%s stdout)", msg);
+
+		if (execvp(args[0], args) == -1)
+	       		err(5, "could not execute execvp");	
+	}
+	return pid;	
+}	
 
 int main(int argc, char** argv)
 {
 	if (argc != 2)
-		errx(42, "usage: %s [dirname]", argv[0]);
-
-	struct stat st;
-
-	if (stat(argv[1], &st) == -1) 
-		err(1, "could not stat");
-
-	if (!S_ISDIR(st.st_mode))
-		errx(2, "%s is not a directory", argv[1]);
+		errx(1, "usage: ./main [dirname]");
 
 	int a[2];
 	if (pipe(a) == -1)
-		err(3, "could not pipe");
+		err(2, "could not pipe");
 
-	pid_t pid1 = fork();
-	if (pid1 == -1)
-		err(4, "could not fork");
+	char* args[] = { "find", argv[1], "-type", "f", "-printf", "%T@ %P\n", NULL };
 
-	if (pid1 == 0)
-	{
-		close(a[0]);
-		if (dup2(a[1], 1) == -1)
-			err(5, "could not execute dup2");
-		if (execlp("find", "find", argv[1], "-type", "f", "-printf", "%T@ %P\n", 0) == -1)
-			err(6, "could not execute execlp");
-	}
-
+	pid_t pid1, pid2, pid3, pid4;
+	pid1 = executeCommand(args, -1, a[1], a[0], "1");
 	close(a[1]);
 
 	int b[2];
 	if (pipe(b) == -1)
-		err(3, "could not pipe");
+		err(2, "could not pipe");
+	char* args2[] = { "sort", "-rn", NULL };
 
-	pid_t pid2 = fork();
-	if (pid2 == -1)
-		err(4, "could not fork");
-
-	if (pid2 == 0)
-	{
-		if (dup2(a[0], 0) == -1)
-			err(5, "could not execute dup2");
-		close(b[0]);
-		if (dup2(b[1], 1) == -1)
-			err(5, "could not execute dup2");
-		if (execlp("sort", "sort", "-rn", 0) == -1)
-			err(6, "could not execute execlp");
-	}
-	
+	pid2 = executeCommand(args2, a[0], b[1], b[0], "2");
+	close(a[0]);
 	close(b[1]);
 
 	int c[2];
 	if (pipe(c) == -1)
-		err(3, "could not pipe");
+		err(2, "could not pipe");
 
-	pid_t pid3 = fork();
-	if (pid3 == -1)
-		err(4, "could not fork");
+	char* args3[] = { "head", "-1", NULL };
 
-	if (pid3 == 0)
-	{
-		if (dup2(b[0], 0) == -1)
-			err(5, "could not execute dup2");
-		close(c[0]);
-		if (dup2(c[1], 1) == -1)
-			err(5, "could not execute dup2");
-		if (execlp("head", "head", "-1", 0) == -1)
-			err(6, "could not execute execlp");
-	}
-
+	pid3 = executeCommand(args3, b[0], c[1], c[0], "3");
+	close(b[0]);
 	close(c[1]);
 
-	if (dup2(c[0], 0) == -1)
-		err(5, "could not execute dup2");
-	if (execlp("cut", "cut", "-d", " ", "-f2", 0) == -1)
-		err(6, "could not execute execlp");
+	char* args4[] = { "cut", "-d", " ", "-f2", NULL };
+
+	pid4 = executeCommand(args4, c[0], -1, -1, "4");
+	close(c[0]);
+
+	int status;
+ 	if (waitpid(pid1, &status, 0) == -1)
+		err(3, "error in wait");
+
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		errx(6, "command failed");
+
+	if (waitpid(pid2, &status, 0) == -1)
+		err(3, "error in wait");
+
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		errx(6, "command failed");
+
+	if (waitpid(pid3, NULL, 0) == -1)
+		err(3, "error in wait");	
+	
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		errx(6, "command failed");
+
+	if (waitpid(pid4, NULL, 0) == -1)
+		err(3, "error in wait");
+
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		errx(6, "command failed");
+
+	exit(0);
+
 }
