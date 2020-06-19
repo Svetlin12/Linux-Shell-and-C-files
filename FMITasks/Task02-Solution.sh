e88999629bf4b6bc2f7ae000d37749d5370e3d39
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-06-19 18:39:51 +0300

    Update Task02-Solution.sh

--- FMITasks/Task02-Solution.sh
+++ FMITasks/Task02-Solution.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
-	echo "Error: expected 1 argument!"
+	echo "Invalid number of arguments! Usage: $0 (number)"
 	exit 1
 fi
 
-if [ $(id -u) -ne 0 ]; then
-	echo "Error: expected root as caller!"
+NUM=$1
+
+if [ $(egrep -c "^[1-9][0-9]*" <(echo "${NUM}")) -eq 0 ]; then
+	echo "Argument should be a positive number"
 	exit 2
 fi
 
-AIMEDAMOUNT=$1
-ALLUSERS=$(ps -e -o user= | sort | uniq)
+if [ $(id -u) -ne 0 ]; then
+	echo "Expected root as caller"
+	exit 3
+fi
 
-for USR in ${ALLUSERS}; do
-	
-	TOTALSIZE=$(ps -u "${USR}" -o rss= | sort -n | awk '{SZ += $1} END{print SZ}')
-	
-	if [ ${TOTALSIZE} -gt ${AIMEDAMOUNT} ]; then
-		TOKILL=$(ps -u  "${USR}" -o rss=,pid= | sort -n | tail -n 1 | awk '{print $2}')
-		kill -s TERM ${TOKILL}
-		sleep 3
-		kill -s KILL ${TOKILL}
+while read _USER; do
+	RSS_MAX=$(awk 'BEGIN{sum=0} { sum += $1; if (max < $1) {max = $1; pid=$2} } END{print sum " " max}' <(ps -u "${_USER}" -o rss=,pid=))
+	TOTAL_RSS=$(echo "${RSS_MAX}" | cut -d " " -f1)
+	MAX_PID=$(echo "${RSS_MAX}" | cut -d " " -f3)
+	if [ "${TOTAL_RSS}" -gt "${NUM}" ]; then
+		#kill -15 "${MAX_PID}"
+		#sleep 2
+		#kill -9 "${MAX_PID}"
 	fi
-
-done	
+done < <(cut -d : -f1 /etc/passwd)
