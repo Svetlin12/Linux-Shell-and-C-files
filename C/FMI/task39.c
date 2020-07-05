69b9b7a5fd5711b41e804306f95cf27381d58d15
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-07-05 23:37:17 +0300

    Update task39.c

--- C/FMI/task39.c
+++ C/FMI/task39.c
@@ -1,178 +1,159 @@
 #include <err.h>
 #include <errno.h>
-#include <stdint.h>
-#include <sys/types.h>       
-#include <sys/stat.h>      
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdint.h>
 
-void errorHandler(int code, int errnum, int fd1, int fd2, int fd3, int fd4, uint32_t* buffer)
+void errHandler(int code, int errnum, int fd1, int fd2, int fd3, void* buff, const char* msg)
 {
-	if (fd1 != -1)
-		close(fd1);
-
-	if (fd2 != -1)
-		close(fd2);
-
-	if (fd3 != -1)
-		close(fd3);
-
-	if (fd4 != -1)
-		close(fd4);
-
-	free(buffer);
+	if (fd1 != -1) close(fd1);
+	if (fd2 != -1) close(fd2);
+	if (fd3 != -1) close(fd3);
+	free(buff);
 	errno = errnum;
-	err(code, "error code %d", code);
+	err(code, "%s", msg);
 }
 
 int cmp(const void* a, const void* b)
 {
-	if ((*(uint32_t*)a) < (*(uint32_t*)b))
+	if (*(uint32_t*)a < *(uint32_t*)b)
 		return -1;
-	else if ((*(uint32_t*)a) == (*(uint32_t*)b))
+	else if (*(uint32_t*)a == *(uint32_t*)b)
 		return 0;
-	else 
-		return 1;
+	else return 1;
 }
 
 int main(int argc, char** argv)
 {
 	if (argc != 3)
-		errx(1, "usage: ./main input.bin output.bin");
-
-	if (access(argv[1], F_OK) == -1)
-		errx(2, "%s is not a file", argv[1]);
-
-	if (access(argv[1], R_OK) == -1)
-		errx(3, "file %s is not readable", argv[1]);
-
-	if (access(argv[2], F_OK) != -1 && access(argv[2], W_OK) == -1)
-		errx(4, "file %s is not writable", argv[2]);
+		errx(1, "Invalid argument count. Usage: %s (input.bin) (output.bin)", argv[0]);
 	
-	int input = open(argv[1], O_RDONLY);
-	if (input == -1)
-		errorHandler(5, errno, input, -1, -1, -1, NULL);
-
-	int output = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
-	if (output == -1)
-		errorHandler(6, errno, input, output, -1, -1, NULL);
-
-	int temp1 = open("temp1", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-	if (temp1 == -1)
-		errorHandler(7, errno, input, output, temp1, -1, NULL);
-
 	struct stat st;
 	if (stat(argv[1], &st) == -1)
-		errorHandler(7, errno, input, output, temp1, -1, NULL);
+		err(2, "Could not stat %s", argv[1]);
+	
+	if (!(S_ISREG(st.st_mode)))
+		errx(3, "%s is not a regular file", argv[1]);
+	
+	if (!(st.st_mode & S_IRUSR))
+		errx(4, "%s is not readable", argv[1]);
 
 	if (st.st_size % sizeof(uint32_t) != 0)
-		errx(8, "file %s does not consist of only uint32_t type numbers", argv[1]);
+		errx(5, "%s does not consist only of uint32_t numbers", argv[1]);
 
 	uint32_t numElements = st.st_size / sizeof(uint32_t);
-	uint32_t leftHalf = numElements / 2;
-	uint32_t rightHalf = numElements - leftHalf;
+	uint32_t leftSideCnt = numElements / 2;
+	uint32_t rightSideCnt = numElements - leftSideCnt;
 
-	uint32_t* buffer = malloc(leftHalf * sizeof(uint32_t));
+	int source = open(argv[1], O_RDONLY);
+	if (source == -1)
+		err(6, "Could not open file %s", argv[1]);
+
+	int temp1 = open("temp1", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+	if (temp1 == -1)
+		errHandler(7, errno, source, temp1, -1, NULL, "Could not open first temp file");
+
+	uint32_t* buffer = malloc(leftSideCnt * sizeof(uint32_t));
 	if (buffer == NULL)
-		errorHandler(9, errno, input, output, temp1, -1, NULL);
+		errHandler(8, errno, source, temp1, -1, NULL, "No space left to allocate");	
 
-	size_t res = read(input, buffer, leftHalf * sizeof(uint32_t));
-	if (res != leftHalf * sizeof(uint32_t))
-		errorHandler(10, errno, input, output, temp1, -1, buffer);
+	size_t size = read(source, buffer, leftSideCnt * sizeof(uint32_t));
+	if (size != leftSideCnt * sizeof(uint32_t))
+		errHandler(9, errno, source, temp1, -1, buffer, "Error occurred while reading from soruce file");
 
-	qsort(buffer, leftHalf, sizeof(uint32_t), cmp);
-	res = write(temp1, buffer, leftHalf * sizeof(uint32_t));
-	if (res != leftHalf * sizeof(uint32_t))
-		errorHandler(11, errno, input, output, temp1, -1, buffer);
-	
-	free(buffer);	
+	qsort(buffer, leftSideCnt, sizeof(uint32_t), cmp);
 
-	int temp2 = open("temp2", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-	if (temp2 == -1) 
-		errorHandler(12, errno, input, output, temp1, temp2, NULL);
-	
-	buffer = malloc(rightHalf * sizeof(uint32_t));
-	if (buffer == NULL)
-		errorHandler(13, errno, input, output, temp1, temp2, NULL);
-	
-	res = read(input, buffer, rightHalf * sizeof(uint32_t));
-       	if (res != rightHalf * sizeof(uint32_t))
-		errorHandler(14, errno, input, output, temp1, temp2, buffer);
+	size = write(temp1, buffer, leftSideCnt * sizeof(uint32_t));
+	if (size != leftSideCnt * sizeof(uint32_t))
+		errHandler(10, errno, source, temp1, -1, buffer, "Error occurred while writing to first temporary file");
 
-	qsort(buffer, rightHalf, sizeof(uint32_t), cmp);
-	res = write(temp2, buffer, rightHalf * sizeof(uint32_t));
-	if (res != rightHalf * sizeof(uint32_t))
-		errorHandler(15, errno, input, output, temp1, temp2, buffer);
-	
-	close(input);
 	free(buffer);
 
-	lseek(temp1, 0, SEEK_SET);
-	lseek(temp2, 0, SEEK_SET);
+	if (lseek(temp1, 0, SEEK_SET) == -1)
+		errHandler(11, errno, source, temp1, -1, NULL, "Error occurred while lseek-ing first temporary file");
 
-	uint32_t leftNum;
-	uint32_t rightNum;
+	buffer = malloc(rightSideCnt * sizeof(uint32_t));
+	if (buffer == NULL)
+		errHandler(12, errno, source, temp1, -1, NULL, "No space left to allocate");
 
-	if (read(temp1, &leftNum, sizeof(leftNum)) != sizeof(leftNum))
-		errorHandler(16, errno, -1, output, temp1, temp2, NULL);
+	int temp2 = open("temp2", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+	if (temp2 == -1)
+		errHandler(13, errno, source, temp1, temp2, buffer, "Could not open second temp file");
 
-	if (read(temp2, &rightNum, sizeof(rightNum)) != sizeof(rightNum))
-		errorHandler(17, errno, -1, output, temp1, temp2, NULL);
+	size = read(source, buffer, rightSideCnt * sizeof(uint32_t));
+	if (size != rightSideCnt * sizeof(uint32_t))
+		errHandler(14, errno, source, temp1, temp2, buffer, "Error occurred while reading from source file");
 
-	uint32_t leftCnt = 0;
-	uint32_t rightCnt = 0;
-	ssize_t readSize;
-	while (leftCnt < leftHalf && rightCnt < rightHalf)
+	qsort(buffer, rightSideCnt, sizeof(uint32_t), cmp);
+
+	size = write(temp2, buffer, rightSideCnt * sizeof(uint32_t));
+	if (size != rightSideCnt * sizeof(uint32_t))
+		errHandler(15, errno, source, temp1, temp2, buffer, "Error occurred while writing to second temp file");
+
+	free(buffer);
+	close(source);
+	
+	if (lseek(temp2, 0, SEEK_SET) == -1)
+		errHandler(16, errno, -1, temp1, temp2, NULL, "Error occurred while lseek-ing second temp file");
+
+	int dest = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (dest == -1)
+		errHandler(17, errno, dest, temp1, temp2, NULL, "Could not open destination file");
+
+	ssize_t rd1, rd2;
+	uint32_t left, right;
+	while ((rd1=read(temp1, &left, sizeof(left))) > 0 && ((rd2=read(temp2, &right, sizeof(right))) > 0))
 	{
-		if (leftNum < rightNum)
+		if (left < right)
 		{
-			if (write(output, &leftNum, sizeof(leftNum)) != sizeof(leftNum))
-				errorHandler(18, errno, -1, output, temp1, temp2, NULL);
-			
-			if ((readSize=read(temp1, &leftNum, sizeof(leftNum))) != sizeof(leftNum))
-				break;
-
-			leftCnt++;
+			if (write(dest, &left, sizeof(left)) != sizeof(left))
+				errHandler(18, errno, dest, temp1, temp2, NULL, "Error occurred while writing to dest file");
+	
+			if (lseek(temp2, -1 * sizeof(uint32_t), SEEK_CUR) == -1)
+				errHandler(19, errno, dest, temp1, temp2, NULL, "Error occurred while lseek-ing temp2 file");
 		}
 		else
 		{
-			if (write(output, &rightNum, sizeof(rightNum)) != sizeof(rightNum))
-				errorHandler(19, errno, -1, output, temp1, temp2, NULL);
-			
-			if ((readSize=read(temp2, &rightNum, sizeof(rightNum))) != sizeof(rightNum))
-			       break;
+			if (write(dest, &right, sizeof(right)) != sizeof(left))
+				errHandler(20, errno, dest, temp1, temp2, NULL, "Error occurred while writing to dest file");
 
-			rightCnt++;	
+			if (lseek(temp1, -1 * sizeof(uint32_t), SEEK_CUR) == -1)
+				errHandler(21, errno, dest, temp1, temp2, NULL, "Error occurred while lseek-ing temp1 file");
 		}
-			
 	}
 
-	if (readSize == -1)
-		errorHandler(20, errno, -1, output, temp1, temp2, NULL);
-
-	while ((readSize=read(temp1, &leftNum, sizeof(leftNum))) == sizeof(leftNum))
+	if (rd1 == sizeof(uint32_t))
 	{
-		if (write(output, &leftNum, sizeof(leftNum)) != sizeof(leftNum))
-			errorHandler(21, errno, -1, output, temp1, temp2, NULL);
+		if (write(dest, &left, sizeof(left)) > 0)
+			errHandler(22, errno, dest, temp1, temp2, NULL, "Error occurred while writing to dest file");
+	} 
 
-	}	
+	if (rd1 == -1 || rd2 == -1)
+		errHandler(23, errno, dest, temp1, temp2, NULL, "Error occurred while reading from temp files");
 
-	if (readSize == -1)
-		errorHandler(22, errno, -1, output, temp1, temp2, NULL);
-
-	while ((readSize=read(temp1, &rightNum, sizeof(rightNum))) == sizeof(rightNum))
+	while ((rd1=read(temp1, &left, sizeof(left))) > 0)
 	{
-		if (write(output, &rightNum, sizeof(rightNum)) != sizeof(rightNum))
-				errorHandler(23, errno, -1, output, temp1, temp2, NULL);
+		if (write(dest, &left, sizeof(left)) != sizeof(left))
+			errHandler(24, errno, dest, temp1, temp2, NULL, "Error occurred while writing to dest file");
 	}
 
-	if (readSize == -1)
-		errorHandler(24, errno, -1, output, temp1, temp2, NULL);
+	while ((rd2=read(temp2, &right, sizeof(right))) > 0)
+	{
+		if (write(dest, &right, sizeof(right)) != sizeof(right))
+			errHandler(25, errno, dest, temp1, temp2, NULL, "Error occurred while writing to dest file");
+	}
 
-	close(output);
+	close(dest);
 	close(temp1);
 	close(temp2);
-	exit(0);	
+
+	if (unlink("temp1") == -1)
+		err(26, "Could not unlink file temp1");
+	
+	if (unlink("temp2") == -1)
+		err(27, "Could not unlink file temp2");
+	exit(0);
 }
