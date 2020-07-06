440c353a47b8a6d9a09cbc0dbea70a678772b9b6
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-07-06 19:10:18 +0300

    Update task40.c

--- C/FMI/task40.c
+++ C/FMI/task40.c
@@ -1,133 +1,149 @@
-#include <unistd.h>
 #include <err.h>
-#include <errno.h>
-#include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int isCorrectDigit(char digit)
+int isInvalidDigit(char digit)
 {
-	if (digit >= '1' && digit <= '9')
+	if (digit < '1' || digit > '9')
 		return 1;
-	else
+	else return 0;
+}
+
+int isValidInterval(char start, char end)
+{
+	if (start > end)
 		return 0;
+	else return 1;
+}
+
+int isValidSeparator(char separator)
+{
+	if (separator != '-')
+		return 0;
+	else return 1;
+}
+
+int getDigit(char digit)
+{
+	return digit - '0';
 }
 
 int main(int argc, char** argv)
 {
-	if (argc == 3 && strcmp(argv[1], "-c") == 0 && strlen(argv[2]) == 1)
+	if (argc == 3 && strcmp(argv[1], "-c") == 0)
 	{
-		char c;
-		int number = (int)(*argv[2]) - '0';
-		int cnt = 0;
-
-		if (!isCorrectDigit(*argv[2]))
-			errx(1, "usage: ./main -c [1-9]");
-
-		while (read(0, &c, 1) > 0)
+		int start, end;
+		if (strlen(argv[2]) == 3)
 		{
-			cnt++;
-
-			if (c == '\n')
-			{
-				write(1, "\n", 1);
-				cnt = 0;
-			}
-			else if (cnt == number)
-				write(1, &c, sizeof(c));
-		}
-	}
-	else if (argc == 3 && strcmp(argv[1], "-c") == 0 && strlen(argv[2]) == 3)
-	{
-		if (argv[2][0] > argv[2][2])
-			errx(2, "expected interval: [1-9]-[1-9]");
-
-		if (!isCorrectDigit(argv[2][0]) || !isCorrectDigit(argv[2][2]) || argv[2][1] != '-')
-			errx(3, "usage: ./main -c [1-9]-[1-9]");
-
-		char first = (int)argv[2][0] - '0';
-		char last = (int)argv[2][2] - '0';
-		char c;
-		int cnt = 0;
-		while (read(0, &c, 1) > 0)
-		{
-			cnt++;
-			if (c == '\n')
-			{
-				write(1, &c, sizeof(c));
-				cnt = 0;
-			}
-			else if (cnt >= first && cnt <= last)
-				write(1, &c, sizeof(c));
-		}
-	}	
-
-	else if (argc == 5 && strcmp(argv[1], "-d") == 0 && strlen(argv[4]) == 1)
-	{
-		if (strcmp(argv[3], "-f") != 0)
-		       errx(4, "expected -f after delimiter");
+			if (isInvalidDigit(argv[2][0]))
+				errx(1, "Invalid start of interval");
 	
-		if (!isCorrectDigit(*argv[4]))	
-			errx(5, "expected number of column");
-
-		char delimiter = argv[2][0];
-
-		int numColumn = (int)(*argv[4]) - '0';
-		int cnt = 1;
-		char c;
-		while (read(0, &c, 1) > 0)
-		{
-			if (c == delimiter)
-				cnt++;
+			if (isInvalidDigit(argv[2][2]))
+				errx(2, "Invalid end of interval");
 		
-			if (c == '\n')
-			{
-				write(1, "\n", 1);
-				cnt = 1;
-			}
-			else if (cnt == numColumn && c != delimiter)
-				write(1, &c, sizeof(c));
+			if (!isValidInterval(argv[2][0], argv[2][2]))
+				errx(3, "Start of interval is bigger than end of interval");
+
+			if (!isValidSeparator(argv[2][1]))
+				errx(4, "Invalid separator for the interval");
+		
+			start = getDigit(argv[2][0]);
+			end = getDigit(argv[2][2]);
 		}
-	}
-	else if (argc == 5 && strcmp(argv[1], "-d") == 0 && strlen(argv[4]) == 3)
-	{
-		if (strcmp(argv[3], "-f") != 0)
-			errx(6, "expected -f after delimiter");
-	
-		if (!isCorrectDigit(argv[4][0]) || !isCorrectDigit(argv[4][2]) || argv[4][1] != '-')	
-			errx(7, "incorrect interval");
-
-		char delimiter = argv[2][0];
-
-		int firstColumn = (int)argv[4][0] - '0';
-		int lastColumn = (int)argv[4][2] - '0';
-
-		if (firstColumn > lastColumn)
-			errx(8, "start of interval is bigger than end");
-
-		int cnt = 1;
+		else if (strlen(argv[2]) == 1)
+		{
+			if (isInvalidDigit(argv[2][0]))
+				errx(5, "Invalid number. Valid numbers are 1-9.");
+		
+			start = getDigit(argv[2][0]);
+			end = getDigit(argv[2][0]);
+		}
+		else
+			errx(6, "Invalid interval after option -c. Usage: %s -c [1-9]\n%s -c [1-9]-[1-9]", argv[0], argv[0]);
+		
 		char c;
+		int cnt = 1;
 		while (read(0, &c, 1) > 0)
 		{
+			if ((cnt >= start && cnt <= end) || c == '\n')
+				write(1, &c, 1);
+			cnt++;
+		}
+	}
+	else if (argc == 5 && strcmp(argv[1], "-d") == 0)
+	{
+		char delimiter = argv[2][0];
+		if (strcmp(argv[3], "-f") != 0)
+			errx(7, "Expected -f as third argument");
+
+		int start, end;
+		if (strlen(argv[4]) == 3)
+		{
+			if (isInvalidDigit(argv[4][0]))
+				errx(8, "Invalid start of interval");
+
+			if (isInvalidDigit(argv[4][2]))
+				errx(9, "Invalid end of interval");
+
+			if (!isValidInterval(argv[4][0], argv[4][2]))
+				errx(10, "Start of interval is bigger than end of interval");
+
+			if (!isValidSeparator(argv[4][1]))
+				errx(11, "Invalid separator for the interval");
+
+			start = getDigit(argv[4][0]);
+			end = getDigit(argv[4][2]);
+		}
+		else if (strlen(argv[4]) == 1)
+		{
+			if (isInvalidDigit(argv[4][0]))
+				errx(12, "Invalid number. Valid numbers are 1-9.");
+
+			start = getDigit(argv[4][0]);
+			end = getDigit(argv[4][0]);			
+		}
+		else 
+			errx(13, "Invalid fourth argument. Usage: %s -d (delimiter) -f [1-9]\n%s -d (delimiter) -f [1-9]-[1-9]", argv[0], argv[0]);
+
+		char c;
+		int cnt = 1;
+		while (read(0, &c, 1) > 0)
+		{
+			if (start == end && cnt == start)
+			{
+				if (c != delimiter)
+					write(1, &c, 1);
+				else
+					cnt++;
+				
+				if (c == '\n')
+					cnt = 1;
+				
+				continue;
+			}
+		
+			if (cnt == end && c == delimiter)
+			{
+				cnt++;
+				if (c == '\n')
+					cnt = 1;
+				continue;
+			}
+
+			if ((cnt >= start && cnt <= end) || c == '\n')
+				write(1, &c, 1);
+
 			if (c == delimiter)
 				cnt++;
-
+	
 			if (c == '\n')
-			{
-				write(1, "\n", 1);
 				cnt = 1;
-			}
-			else if (cnt >= firstColumn && cnt <= lastColumn)
-			{
-				if (cnt == firstColumn && c == delimiter)
-					continue;	
-				write(1, &c, sizeof(c));
-			}
 		}
 	}
 	else
-		errx(9, "usage: ./main -c [1-9]\n./main -c [1-9]-[1-9]\n./main -d [char] -f [1-9]\n./main -d [char] -f [1-9]-[1-9]");
-	
+		errx(14, "Invalid argument count. Usage: %s -c (interval or a single number)\n%s -d (delimiter) -f (interval or a single number)", argv[0], argv[0]);
+
 
 	exit(0);
 }
