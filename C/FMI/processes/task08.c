588a3f996cf76d9fca22b8c6422021e532e0bfdd
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-07-07 18:29:09 +0300

    Update task08.c

--- C/FMI/processes/task08.c
+++ C/FMI/processes/task08.c
@@ -1,58 +1,72 @@
-#include <stdlib.h>
 #include <err.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
-void errorHandler(int code, int errnum, int fd)
-{	
-	if (fd != -1)
-		close(fd);
-	errno = errnum;
-	err(code, "error code: %d", code);
-}
-
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
 	if (argc != 2)
-		errx(1, "usage: %s [filename]", argv[0]);
+		errx(1, "Invalid argument count. Usage: %s (filename)", argv[0]);
 
 	int fileFd = open(argv[1], O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fileFd == -1)
-		err(2, "could not open file %s", argv[1]);
+		err(2, "Could not open file %s", argv[1]);
 
 	pid_t pid = fork();
 	if (pid == -1)
-		errorHandler(3, errno, fileFd);
+	{
+		int olderrno = errno;
+		close(fileFd);
+		errno = olderrno;
+		err(3, "Could not fork");
+	}
 
 	if (pid == 0)
 	{
-		char foo[7] = "foobar\n";
-		if (write(fileFd, &foo, 7) != 7)
-			errorHandler(4, errno, fileFd);
+		if (write(fileFd, "foobar", 6) == -1)
+		{
+			int olderrno = errno;
+			close(fileFd);
+			errno = olderrno;
+			err(4, "Error occurred while writing to file %s", argv[1]);
+		}
+		
+		close(fileFd);	
 		exit(0);
-	}		
+	}
+	
+	int status;
+	if (wait(&status) == -1)
+		err(5, "Error occurred while waiting for child process to finish");
 
-	if (wait(NULL) == -1)
-		errorHandler(5, errno, fileFd);
+	if (!WIFEXITED(status) || WEXITSTATUS(status))
+		err(6, "Child did not end successfully");
 
+	close(fileFd);
 	fileFd = open(argv[1], O_RDONLY);
-	if (fileFd == -1)
-		errorHandler(6, errno, fileFd);
 
-	char c[2];
+	char buff[2];
 	ssize_t readSize;
-	while ((readSize=read(fileFd, &c, 2)) > 0)
+	while ((readSize=read(fileFd, &buff, sizeof(buff))) > 0)
 	{
-		write(1, &c, readSize);
-		write(1, " ", 1);	
-	} 
-
+		write(1, &buff, readSize);
+		write(1, " ", 1);
+	}
+	
 	if (readSize == -1)
-		errorHandler(7, errno, fileFd);	
+	{
+		int olderrno = errno;
+		close(fileFd);
+		errno = olderrno;
+		err(7, "Error occurred while reading from file %s", argv[1]);
+	}
 
+	write(1, "\n", 1);
+	
+	close(fileFd);
 	exit(0);
 }
