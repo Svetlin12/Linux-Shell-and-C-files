a57e291032c2d4f9671e50bf7da78801dafd8b9d
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-07-08 17:35:35 +0300

    Update task10.c

--- C/FMI/processes/task10.c
+++ C/FMI/processes/task10.c
@@ -1,47 +1,48 @@
-#include <stdlib.h>
 #include <err.h>
-#include <errno.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
 	if (argc != 3)
-		errx(1, "usage: ./main [command1] [command2]");
+		errx(1, "Invalid argument count. Usage: %s (command 1) (command 2)", argv[0]);
 
 	pid_t pid1 = fork();
-	if (pid1 == -1)
-		err(2, "could not fork");
 
+	if (pid1 == -1)
+		err(2, "Could not fork");
+	
 	if (pid1 == 0)
 	{
 		if (execlp(argv[1], argv[1], 0) == -1)
-			err(3, "could not execute command %s", argv[1]);
+			err(3, "Could not execute execlp");
 	}
-
+	
 	int status;
 	if (wait(&status) == -1)
-		err(4, "error occurred in wait");
-
-	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
-	{
-		warnx("child process did not end normally");
-		exit(-1);	
-	}
-
+		err(4, "Error while waiting for child to end");
+	
+	if (!WIFEXITED(status) || WEXITSTATUS(status))
+		errx(-1, "Child did not end successfully");
+	
 	pid_t pid2 = fork();
+	
 	if (pid2 == -1)
-		err(5, "could not fork again");
+		err(5, "Could not fork");
 
 	if (pid2 == 0)
 	{
 		if (execlp(argv[2], argv[2], 0) == -1)
-			err(6, "could not execute command %s", argv[2]);
+			err(6, "Could not execute execlp");
 	}
 
-	if (wait(NULL) == -1)
-		err(7, "error occurred in wait");
+	if (wait(&status) == -1)
+		err(7, "Error while waiting for child to end");
+
+	if (!WIFEXITED(status) || WEXITSTATUS(status))
+		errx(-1, "Child did not end successfully");
 
 	exit(0);
 }
